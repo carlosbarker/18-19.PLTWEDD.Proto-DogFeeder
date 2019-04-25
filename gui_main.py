6908f9e661e9481fc50f997879156cbb3b7ea2b2
carlosbarker <carlosbarker@outlook.com>
2019-04-24 22:24:59 -0600

    restart window work

--- gui_main.py
+++ gui_main.py
@@ -1,6 +1,6 @@
-from guizero import App, PushButton, Box, Text
+from guizero import App, PushButton, Box, Text, Window
 
-app = App(title="Dog Feeder", layout="grid", width="640", height="480", bg="white")
+app = App(title="Dog Feeder", layout="grid", width="800", height="480", bg="white")
 # app.set_full_screen() WILL USE THIS WHEN I HAVE SCREEN
 
 
@@ -9,8 +9,20 @@ def settings():
 
 
 def restart_prompt():
+    restart_window = Window(app, height=100, width=500)
+    text = Text(restart_window, text="Would you like to restart the entire system or just the interface?")
+    restart_device_button = PushButton(restart_window, command=restart_device, text="Restart Device")
+    restart_interface_button = PushButton(restart_window, command=restart_interface, text="Restart Interface")
+
+def restart_interface():
     return ''
 
+def restart_device():
+    command = "/usr/bin/sudo /sbin/shutdown -r now"
+    import subprocess
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output=process.communicate()[0]
+    print(output)
 
 menu_box = Box(app, layout="grid", grid=[0, 0], width=100, height=480)
 settings_button = PushButton(menu_box, command=settings, text="Settings", grid=[0, 0], width=10)
